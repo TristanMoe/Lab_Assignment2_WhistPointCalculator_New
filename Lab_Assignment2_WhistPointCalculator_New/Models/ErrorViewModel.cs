4e4d5be70c4500de11924af6e1757028b6f7a845
TristanMoe <Trilleo@hotmail.com>
2019-04-01 10:21:48 +0200

    Add project files.

---
+++ Lab_Assignment2_WhistPointCalculator_New/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab_Assignment2_WhistPointCalculator_New.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
