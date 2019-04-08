55128ebaaacb2ffbabd4b7114a758cf1ee202d2b
Martin <martinx1995x@hotmail.com>
2019-04-08 11:43:58 +0200

    gamesController changes

--- Lab_Assignment2_WhistPointCalculator_New/DataSeeder.cs
+++ Lab_Assignment2_WhistPointCalculator_New/DataSeeder.cs
@@ -14,6 +14,39 @@ namespace Lab_Assignment2_WhistPointCalculator.DAL
                     FirstName = "Marcus",
                     LastName = "Gasberg",
                     GamePlayers = new List<GamePlayers>(),
+                }
+                ,
+                new Players()
+                {
+                    PlayerId = 2,
+                    FirstName = "Martin",
+                    LastName = "Jespersen",
+                    GamePlayers = new List<GamePlayers>(),
+                });
+
+            modelBuilder.Entity<GamePlayers>().HasData(
+                new GamePlayers()
+                {
+                    GamesId = 1,
+                    PlayerId = 1,
+                    Points = 4,
+                    PlayerPosition = 1
+
+                },
+                new GamePlayers()
+                {
+                    GamesId = 1,
+                    PlayerId = 2,
+                    Points = 3,
+                    PlayerPosition = 2
+                });
+            modelBuilder.Entity<Games>().HasData(
+                new Games()
+                {
+                    GamesId = 1,
+                    Name = "SuperWeebTanks",
+                    Ended = false
+
                 });
         }
     }
